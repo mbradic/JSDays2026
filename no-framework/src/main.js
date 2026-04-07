6dba2cb86b7bbfb0442b3f139dfb26f36501e851
Miroslav Bradic <mbradic@yahoo.com>
2026-04-07 12:30:49 +0200

    no-framework-live

--- no-framework/src/main.js
+++ no-framework/src/main.js
@@ -1,158 +1,145 @@
 class StateManager {
-  #state = {};
-
+  #state = {
+    todos: [
+        {
+            id: 1,
+            title: 'aaa',
+            completed: false
+        }
+    ],
+    newToDo: ''
+  };
   get state() {
-    return this.#state;
+    return {...this.#state};
   }
-
-  setState(newState) {
+  set state(newState) {
     this.#state = { ...this.#state, ...newState };
-    this.#notifyChange();
+    this.#notifyStateChanged();
   }
 
-  #changeCallbacks = [];
-
-  subscribe(callback) {
-    this.#changeCallbacks.push(callback);
+  #stateChangedCallbacks = [];
+  subscribeStateChanged(cb) {
+    this.#stateChangedCallbacks.push(cb);
   }
-
-  unsubscribe(callback) {
-    this.#changeCallbacks = this.#changeCallbacks.filter(
-      (cb) => cb !== callback,
+  unsubscribeStateChanged(cb) {
+    this.#stateChangedCallbacks = this.#stateChangedCallbacks.filter(
+      (c) => c !== cb,
     );
   }
 
-  #notifyChange() {
-    this.#changeCallbacks.forEach((callback) => callback(this.#state));
+  #notifyStateChanged() {
+    for(const cb of this.#stateChangedCallbacks) cb({...this.#state})
   }
 }
 
 class Router {
-  constructor(routes, stateManager) {
-    this.routes = routes;
-    this.stateManager = stateManager;
-    window.addEventListener("hashchange", this.handleRouteChange.bind(this));
-  }
+    constructor(routes, stateManager) {
+        this.routes = routes
+        this.stateManager = stateManager
+        window.addEventListener("hashchange", this.handleHashChange.bind(this))
+    }
 
-  navigate(path) {
-    const route = this.routes.find((r) => r.path === path);
-    if (route && route.loader) {
-      route.loader(this.stateManager);
+    navigate(path) {
+        location.hash = path
     }
-    if (route && route.action) {
-      route.action(this.stateManager);
-    }
-    window.location.hash = path;
-  }
 
-  handleRouteChange() {
-    console.log("Route changed:", window.location.hash);
-    const path = window.location.hash.slice(1);
-    const route = this.routes.find((r) => r.path === path);
-    if (route) {
-      new route.component(this.stateManager).render();
-    }
-  }
+    handleHashChange() {
+            const outlet = document.getElementById("outlet")
+            const hash = location.hash.split('#')[1] ?? ""
+            const route = this.routes.find(r => r.path === hash)
+            const componentInstance = new route.component(stateManager, this)
+            componentInstance.render(outlet)
+            if(route.loader) {
+                route.loader(this.stateManager)
+            }
+        }
 }
 
 class Component {
-  constructor(stateManager) {
-    this.stateManager = stateManager;
-    this.stateManager.subscribe(this.render.bind(this));
-  }
-
-  render() {
-    throw new Error("Render method must be implemented by subclass");
-  }
-}
-
-class App extends Component {
-  constructor(stateManager) {
-    super(stateManager);
-    this.stateManager.setState({
-      title: "Todo List",
-      todos: [],
-    });
-  }
-
-  render() {
-    const { title } = this.stateManager.state;
-    const appElement = document.getElementById("app");
-    appElement.innerHTML = `
-    <h1>${title}</h1>
-    <div id="outlet" />
-    `;
-  }
+    constructor(stateManager, router) {
+        this.stateManager = stateManager
+        this.router = router
+        stateManager.subscribeStateChanged(() => { this.render() })
+    }
+    render() {
+        throw new Error('Overide in derived classes')
+    }
 }
 
 class ToDoList extends Component {
-  render() {
-    const { todos } = this.stateManager.state;
-    const outlet = document.getElementById("outlet");
-    outlet.innerHTML = `
-      <h2>List</h2>
-      <ul>
-        ${todos.map((todo) => `<li>${todo.title}</li>`).join("")}
-      </ul>
-      <button id="create-btn">Create Todo</button>
-    `;
-
-    document.getElementById("create-btn").addEventListener("click", () => {
-      router.navigate("create");
-    });
-  }
+    // constructor(stateManager, router) {
+    //     super(stateManager, router)
+    // }
+    render() {
+        const outlet = document.getElementById('outlet')
+        outlet.innerHTML = `
+            <h2>List</h2>
+            <button id="add-button">Add</button>
+            <ul>
+            ${this.stateManager.state.todos.map(todo => `<li>${todo.title}</li>`).join("")}
+            </ul>
+        `
+        const addButton = document.getElementById('add-button')
+        addButton.addEventListener("click", () => {
+            this.router.navigate("new")
+        })
+    }
 }
 
 class ToDoForm extends Component {
-  render() {
-    const outlet = document.getElementById("outlet");
-    outlet.innerHTML = `
-      <input type="text" value="${this.stateManager.state.newTodo || ""}" id="todo-input" placeholder="Enter todo" />
-      <button id="add-btn">Add Todo</button>
-    `;
+    render() {
+        const outlet = document.getElementById('outlet')
+        outlet.innerHTML = `
+            <h2>New todo</h2>
+            <form id="todo-form">
+                <input value="${this.stateManager.state.newToDo}" id="new-todo-input" name="new-todo" />
+                <button>Save</button>
+            </form>
+        `
 
-    document.getElementById("todo-input").addEventListener("input", (e) => {
-      this.stateManager.setState({ newTodo: e.target.value });
-    });
+        const todoForm = document.getElementById('todo-form')
+        todoForm.addEventListener("submit", async (e) => {
+            e.preventDefault()
+            await fetch('http://localhost:3000/api/todos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: this.stateManager.state.newToDo,
+                    completed: false
+                })
+            })
+            this.router.navigate("")
+        })
 
-    document.getElementById("add-btn").addEventListener("click", async () => {
-      const input = document.getElementById("todo-input");
-      const newTodo = input.value.trim();
-      if (newTodo) {
-        const { todos } = this.stateManager.state;
-        this.stateManager.setState({ todos: [...todos, newTodo] });
-        const res = await fetch("http://localhost:3000/api/todos", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ title: stateManager.state.newTodo, completed: false }),
-        });
-        router.navigate("");
-      }
-    });
-  }
+        const input = document.getElementById('new-todo-input')
+        input.addEventListener("change", ()=>{
+            this.stateManager.state = {newToDo: input.value}
+        })
+    }
 }
 
-const stateManager = new StateManager();
-const router = new Router(
-  [
+const stateManager = new StateManager()
+const router = new Router([
     {
-      path: "",
-      component: ToDoList,
-      loader: async (stateManager) => {
-        const res = await fetch("http://localhost:3000/api/todos");
-        const todos = await res.json();
-        stateManager.setState({ todos });
-      },
+        path: "",
+        component: ToDoList,
+        loader: async (stateManager) => {
+            const res = await fetch('http://localhost:3000/api/todos')
+            stateManager.state = {
+                todos: await res.json()
+            }
+        }
     },
     {
-      path: "create",
-      component: ToDoForm,
-      
-    },
-  ],
-  stateManager,
-);
-const app = new App(stateManager, router);
-app.render();
-router.handleRouteChange();
-router.navigate("");
+        path: "new",
+        component: ToDoForm
+    }
+], stateManager)
+router.handleHashChange()
+router.navigate("")
+
+
+
+
